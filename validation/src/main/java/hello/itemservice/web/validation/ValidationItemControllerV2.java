442938eb275d4062f66b76b9a6b437c9ba2aa227
ssang <qkqh34k@gmail.com>
2024-03-18 16:25:43 +0900

    FieldError, ObjectError

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -45,11 +45,18 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-
+        /* BindingResult
+        - 스프링이 제공하는 검증 오류를 보관하는 객체이다. 검증 오류가 발생하면 여기에 보관하면 된다.
+        - BindingResult 가 있으면 @ModelAttribute 에 데이터 바인딩 시 오류가 발생해도 컨트롤러가 호출된다!
+        - BindingResult 가 없으면 400 오류가 발생하면서 컨트롤러가 호출되지 않고, 오류 페이지로 이동한다.
+        - BindingResult 가 있으면 오류 정보( FieldError )를 BindingResult 에 담아서 컨트롤러를 정상 호출한다.
+        */
+        /*BindingResult 는 검증할 대상 바로 다음에 와야한다. 순서가 중요하다.*/
         // 검증 로직
         if(!StringUtils.hasText(item.getItemName())){
+            // 필드에 오류가 있으면 FieldError 객체를 생성해서 bindingResult 에 담아두면 된다.
             bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수 입니다."));
         }
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 100000){
@@ -62,7 +69,48 @@ public class ValidationItemControllerV2 {
         if(item.getPrice() != null && item.getQuantity() != null){
             int resultPrice = item.getPrice() * item.getQuantity();
             if(resultPrice < 10000){
+                // 특정 필드를 넘어서는 오류가 있으면 ObjectError 객체를 생성해서 bindingResult 에 담아두면 된다.
                 bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())){
+            // 두 가지 생성자를 제공한다
+            /*objectName : 오류가 발생한 객체 이름
+            field : 오류 필드
+            rejectedValue : 사용자가 입력한 값(거절된 값)
+            bindingFailure : 타입 오류 같은 바인딩 실패인지, 검증 실패인지 구분 값
+            codes : 메시지 코드
+            arguments : 메시지에서 사용하는 인자
+            defaultMessage : 기본 오류 메시지*/
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null,"상품 이름은 필수 입니다."));
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 100000){
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null,"가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null,"수량은 최대 9,999 까지 허용합니다."));
+        }
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
         //검증에 실패하면 다시 입력 폼으로
