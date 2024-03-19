1e5c5508de13e0b23f93fe805dd778beef8c2c6c
ssang <qkqh34k@gmail.com>
2024-03-19 13:41:19 +0900

    Bean Validation - 스프링 적용과 에러코드

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -100,7 +100,7 @@ public class ValidationItemControllerV2 {
 
         // 검증 로직
         if(!StringUtils.hasText(item.getItemName())){
-            // 두 가지 생성자를 제공한다
+            //FieldError는 두 가지 생성자를 제공한다
             /*objectName : 오류가 발생한 객체 이름
             field : 오류 필드
             rejectedValue : 사용자가 입력한 값(거절된 값)
@@ -167,25 +167,29 @@ public class ValidationItemControllerV2 {
     }
 //    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-        // 컨트롤러에서 BindingResult 는 검증해야 할 객체인 target 바로 다음에 온다. 따라서 BindingResult 는 이미 본인이 검증해야 할 객체인 target 을 알고 있다.
+        // FieldError , ObjectError 는 다루기 너무 번거롭다.
+        // 컨트롤러에서 BindingResult 는 검증해야 할 객체인 target 바로 다음에 온다.
+        // 따라서 BindingResult 는 이미 본인이 검증해야 할 객체인 target 을 알고 있다.
         log.info("objectName={}", bindingResult.getObjectName());
         log.info("target={}", bindingResult.getTarget());
 
         /*BindingResult 가 제공하는 rejectValue() , reject() 를 사용하면
         FieldError , ObjectError 를 직접 생성하지 않고, 깔끔하게 검증 오류를 다룰 수 있다.*/
         /*
-        field : 오류 필드명
-        errorCode : 오류 코드(이 오류 코드는 메시지에 등록된 코드가 아니다.messageResolver를,위한 오류 코드이다.)
-        errorArgs : 오류 메시지에서 {0} 을 치환하기 위한 값
-        defaultMessage : 오류 메시지를 찾을 수 없을 때 사용하는 기본 메시지
+            field : 오류 필드명
+            errorCode : 오류 코드(이 오류 코드는 메시지에 등록된 코드가 아니다.messageResolver를,위한 오류 코드이다.)
+            errorArgs : 오류 메시지에서 {0} 을 치환하기 위한 값
+            defaultMessage : 오류 메시지를 찾을 수 없을 때 사용하는 기본 메시지
         */
 
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.rejectValue("itemName", "required");
-//            1. rejectValue() 호출
-//            2. MessageCodesResolver 를 사용해서 검증 오류 코드로 메시지 코드들을 생성
-//            3. new FieldError() 를 생성하면서 메시지 코드들을 보관
-//            4. th:erros 에서 메시지 코드들로 메시지를 순서대로 메시지에서 찾고, 노출
+            /*
+                1. rejectValue() 호출
+                2. MessageCodesResolver 를 사용해서 검증 오류 코드로 메시지 코드들을 생성
+                3. new FieldError() 를 생성하면서 메시지 코드들을 보관
+                4. th:erros 에서 메시지 코드들로 메시지를 순서대로 메시지에서 찾고, 노출
+            */
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000},null);
---
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -0,0 +1,103 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/v3/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV3 {
+
+    private final ItemRepository itemRepository;
+
+    // 스프링 부트가 spring-boot-starter-validation 라이브러리를 넣으면 자동으로 Bean Validator를 인지하고 스프링에 통합한다.
+    // LocalValidatorFactoryBean 을 글로벌 Validator로 등록한다. 이 Validator는 @NotNull 같은 애노테이션을
+    // 보고 검증을 수행한다. 이렇게 글로벌 Validator가 적용되어 있기 때문에, @Valid , @Validated 만 적용하면 된다.
+    // 검증 오류가 발생하면, FieldError , ObjectError 를 생성해서 BindingResult 에 담아준다.
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v3/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v3/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v3/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        // 검증시 @Validated @Valid 둘다 사용가능하다.
+        // @Validated 는 스프링 전용 검증 애노테이션이고, @Valid 는 자바 표준 검증 애노테이션이다.
+        // 둘중 아무거나 사용해도 동일하게 작동하지만, @Validated 는 내부에 groups 라는 기능을 포함하고 있다.
+
+        // 검증 순서
+        // 1. @ModelAttribute 각각의 필드에 타입 변환 시도
+        //   1. 성공하면 다음으로
+        //   2. 실패하면 typeMismatch 로 FieldError 추가
+        // 2. Validator 적용
+
+        // 바인딩에 성공한 필드만 Bean Validation 적용
+        // BeanValidator는 바인딩에 실패한 필드는 BeanValidation을 적용하지 않는다.
+        // 생각해보면 타입 변환에 성공해서 바인딩에 성공한 필드여야 BeanValidation 적용이 의미 있다.
+        // (일단 모델 객체에 바인딩 받는 값이 정상으로 들어와야 검증도 의미가 있다.)
+
+        // ===================================================================================================
+
+        // BeanValidation 메시지 찾는 순서
+        // 1. 생성된 메시지 코드 순서대로 messageSource 에서 메시지 찾기
+        // 2. 애노테이션의 message 속성 사용 @NotBlank(message = "공백! {0}")
+        // 3. 라이브러리가 제공하는 기본 값 사용.
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v3/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v3/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+}
+
