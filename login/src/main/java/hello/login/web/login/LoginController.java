45384f738eb1010e51f7b4f0662e3d595d825084
ssang <qkqh34k@gmail.com>
2024-03-22 13:41:05 +0900

    custom session 적용

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -18,16 +20,21 @@ import javax.validation.Valid;
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
+
     private final LoginService loginService;
+    private final SessionManager sessionManager;
+
     @GetMapping("/login")
     public String loginForm(@ModelAttribute LoginForm form){
         return "login/loginForm";
     }
-    @PostMapping("/login")
+
+//    @PostMapping("/login")
     public String login(@Valid @ModelAttribute LoginForm form, BindingResult result, HttpServletResponse response){
         if(result.hasErrors()){
             return "login/loginForm";
         }
+
         Member loginMember = loginService.login(form.getLoginId(),form.getPassword());
 
         log.info("login? {}", loginMember);
@@ -43,9 +50,35 @@ public class LoginController {
 
         return "redirect:/";
     }
-    @PostMapping("/logout")
+
+    @PostMapping("/login")
+    public String loginV2(@Valid @ModelAttribute LoginForm form, BindingResult result, HttpServletResponse response){
+        if(result.hasErrors()){
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(),form.getPassword());
+
+        log.info("login? {}", loginMember);
+        if(loginMember == null){
+            result.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 세션 관리자를 통해 세션 생성 후 데이터 보관
+        sessionManager.createSession(loginMember, response);
+
+        return "redirect:/";
+    }
+
+//    @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
         expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request) {
+        sessionManager.expire(request);
         return "redirect:/";
     }
     private void expireCookie(HttpServletResponse response, String cookieName) {
