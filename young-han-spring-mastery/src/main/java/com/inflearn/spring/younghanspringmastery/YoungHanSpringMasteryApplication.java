70085c28a6e3affbd14eade4d4304d4f07ddf8c9
ssang <qkqh34k@gmail.com>
2024-04-01 18:03:22 +0900

    kyh spring web mastery ing

---
+++ young-han-spring-mastery/src/main/java/com/inflearn/spring/younghanspringmastery/YoungHanSpringMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.spring.younghanspringmastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungHanSpringMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungHanSpringMasteryApplication.class, args);
+	}
+
+}
---
+++ young-han-spring-mastery/src/test/java/com/inflearn/spring/younghanspringmastery/YoungHanSpringMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.spring.younghanspringmastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungHanSpringMasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
